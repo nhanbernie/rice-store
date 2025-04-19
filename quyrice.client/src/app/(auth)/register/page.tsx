1797306645c07b455255d244f0a228995701770c
nhanbernie <ttnhanbernie@gmail.com>
2025-04-19 15:22:19 +0700

    feat: register page

--- quyrice.client/src/app/(auth)/register/page.tsx
+++ quyrice.client/src/app/(auth)/register/page.tsx
@@ -1,24 +1,12 @@
 'use client'
-import AuthPage from '@/components/auth-page/AuthPage'
-import { IInputFieldProps } from '@/common/models/input/input.model'
-import AuthForm from '@/components/auth-page/AuthForm'
+import { AuthForm, AuthPage } from '@/components/auth-page'
 
 export default function Register() {
-  const inputField: IInputFieldProps[] = [
-    { label: 'Username', name: 'username', type: 'text', placeholder: 'Enter your username' },
-    { label: 'Email', name: 'email', type: 'email', placeholder: 'Enter your email' },
-    { label: 'Password', name: 'password', type: 'password', placeholder: 'Enter your password' },
-    {
-      label: 'Confirm Password',
-      name: 'confirmPassword',
-      type: 'password',
-      placeholder: 'Confirm your password',
-    },
-  ]
-
   return (
     <AuthPage>
-      <AuthForm inputField={inputField} />
+      <div className="text-start">
+        <AuthForm type={'register'} />
+      </div>
     </AuthPage>
   )
 }
