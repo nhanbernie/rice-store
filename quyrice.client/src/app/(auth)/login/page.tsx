a6da5608a6e799ddf8abba465fb0213a4f7f155f
nhanbernie <ttnhanbernie@gmail.com>
2025-04-19 15:22:31 +0700

    feat: login page

--- quyrice.client/src/app/(auth)/login/page.tsx
+++ quyrice.client/src/app/(auth)/login/page.tsx
@@ -1,3 +1,12 @@
-export default function Login() {
-  return <div>Login</div>
+'use client'
+import { AuthForm, AuthPage } from '@/components/auth-page'
+
+export default function Register() {
+  return (
+    <AuthPage>
+      <div className="text-start">
+        <AuthForm type={'login'} />
+      </div>
+    </AuthPage>
+  )
 }
