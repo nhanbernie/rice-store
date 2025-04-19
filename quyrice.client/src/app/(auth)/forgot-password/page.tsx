dd58680e85b0b556263b4a8d085503ae249937c1
nhanbernie <ttnhanbernie@gmail.com>
2025-04-19 16:14:15 +0700

    feat: forgot password

--- quyrice.client/src/app/(auth)/forgot-password/page.tsx
+++ quyrice.client/src/app/(auth)/forgot-password/page.tsx
@@ -1,3 +1,12 @@
-export default function ForgotPassword() {
-  return <div>Forgot</div>;
+'use client'
+import { AuthForm, AuthPage } from '@/components/auth-page'
+
+export default function Register() {
+  return (
+    <AuthPage>
+      <div className="text-start">
+        <AuthForm type={'forgotPassword'} />
+      </div>
+    </AuthPage>
+  )
 }
