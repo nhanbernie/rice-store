21bfff1de7794ce095146857d67b77f7137ac62c
nhanbernie <ttnhanbernie@gmail.com>
2025-04-30 17:33:14 +0700

    fix: delete font in layout

--- quyrice.client/src/app/layout.tsx
+++ quyrice.client/src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/contexts/themeContext'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Cửa hàng gạo Quý',
