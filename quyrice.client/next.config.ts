7aa40093d4ac121e9d6900510a04b0ac247c0fde
nhanbernie <ttnhanbernie@gmail.com>
2025-05-02 16:17:00 +0700

    refactor(Image nextJs): config type of image

--- quyrice.client/next.config.ts
+++ quyrice.client/next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    formats: ['image/webp', 'image/avif'],
+    minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
 
